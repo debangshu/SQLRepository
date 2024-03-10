1c6b08f5ccc877be7d8e8a9d6611123c70068ff1
Debangshu <dugoswami@hotmail.com>
2024-03-09 22:31:23 -0500

    Update service_account_1.sql

--- service_account_1.sql
+++ service_account_1.sql
@@ -1,9 +1,11 @@
 /* For security reasons the login is created disabled and with a random password. */
 /****** Object:  Login [service_account_1]    Script Date: 3/8/2024 3:08:49 PM ******/
-CREATE LOGIN [service_account_1] WITH PASSWORD=N'FLEvW1kZ1iR/3QY0JYOMKiOoAeHaJH3BmIPMDx8+CEQ='
+CREATE LOGIN [service_account_1] WITH PASSWORD=N'rqGJQ7c8FF0Vckj+NSTjp5mCad8KbpGHR+D8uKYb53s='
 GO
 
-ALTER LOGIN [service_account_1] ENABLE
+ALTER LOGIN [service_account_1] enable
 GO
 
+CREATE USER service_account_1 WITH DEFAULT_SCHEMA=testdb1
+GO
 
