90401af37a206ca54bcf9964e96f1e7a263c7a9f
Debangshu <dugoswami@hotmail.com>
2024-03-09 22:02:29 -0500

    Add files via upload

---
+++ bank_name.sql
@@ -0,0 +1,21 @@
+/****** Object:  Table [dbo].[bank_name]    Script Date: 3/8/2024 3:07:26 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[bank_name](
+	[Bank Name] [varchar](100) NULL,
+	[City] [varchar](100) NULL,
+	[State] [varchar](100) NULL,
+	[Cert] [varchar](100) NULL,
+	[Acquiring Institution] [varchar](100) NULL,
+	[Closing Date] [varchar](100) NULL,
+	[Fund] [varchar](100) NULL
+) ON [PRIMARY]
+GO
+
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON bank_name TO Service_account_1
+GO
---
+++ service_account_1.sql
@@ -0,0 +1,9 @@
+/* For security reasons the login is created disabled and with a random password. */
+/****** Object:  Login [service_account_1]    Script Date: 3/8/2024 3:08:49 PM ******/
+CREATE LOGIN [service_account_1] WITH PASSWORD=N'FLEvW1kZ1iR/3QY0JYOMKiOoAeHaJH3BmIPMDx8+CEQ='
+GO
+
+ALTER LOGIN [service_account_1] ENABLE
+GO
+
+
